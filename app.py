fc553f2cded603927a7b370545d5a48f95d8893e
Tania Rosas <angelina.rosas@gmail.com>
2021-03-18 12:21:56 -0600

    date verification

--- app.py
+++ app.py
@@ -46,9 +46,7 @@ def precipitation():
     session = Session(engine)
     # Perform a query to retrieve the data and precipitation scores
     prcp_data = session.query(Measurement.date, Measurement.prcp).order_by(Measurement.date).all()
-    
     session.close()
-
     # Convert the query results to a dictionary using date as the key and prcp as the value.
     prcp_lists = []
     for date, prcp in prcp_data:
@@ -56,7 +54,6 @@ def precipitation():
         prcp_dic["date"] = date
         prcp_dic["prcp"] = prcp
         prcp_lists.append(prcp_dic)
-
     # Return the JSON representation of your dictionary.
     return jsonify(prcp_lists)
 
@@ -66,12 +63,9 @@ def stations():
     session = Session(engine)
     # Perform a query to retrieve the data and precipitation scores
     results = session.query(Station.name).order_by(Station.name).all()
-    
     session.close()
-
     # Convert list of tuples into normal list
     stations_list = list(np.ravel(results))
-
     # Return a JSON list of stations from the dataset.
     return jsonify(stations_list)
 
@@ -79,12 +73,9 @@ def stations():
 def tobs():
     # Create our session (link) from Python to the DB
     session = Session(engine)
-    
     # Query the dates and temperature observations of the most active station for the last year of data.
-
     # Get latest date
     latest_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
-
     yearago_date = dt.date(2017, 8, 23) - dt.timedelta(days=365)
 
     # Perform a query to retrieve the data and precipitation scores
@@ -92,9 +83,7 @@ def tobs():
         filter(Measurement.station == 'USC00519281').\
         filter(Measurement.date >= yearago_date).\
         order_by(Measurement.date).all()
-
     session.close()
-
     # Convert the query results to a dictionary using date as the key and prcp as the value.
     tobs_year = []
     for date, tobs in results:
@@ -102,89 +91,42 @@ def tobs():
         tobs_dic["date"] = date
         tobs_dic["tobs"] = tobs
         tobs_year.append(tobs_dic)
-
     # Return a JSON list of temperature observations (TOBS) for the previous year.
     return jsonify(tobs_year)
 
 
 @app.route("/api/v1.0/<start>") 
 def start_input(start):
-
     # Create our session (link) from Python to the DB
     session = Session(engine)
-
-    # Get date 
-    date_last = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
-    date_last_str = str(date_last)
-    date_last_str = re.sub("'|,", "",date_last_str)
-    print (date_last_str)
-
-    date_first = session.query(Measurement.date).first()
-    date_first_str = str(date_first)
-    date_first_str = re.sub("'|,", "",date_first_str)
-    print (date_first_str)
-
-
     # Validate start date 
-    valid_date = session.query(exists().where(Measurement.date == start)).scalar()
- 
-    if valid_date:
-
-    	results = (session.query(func.min(Measurement.tobs),func.avg(Measurement.tobs),func.max(Measurement.tobs)).\
-            filter(Measurement.date >= start).all())
-
-    	tob_min =results[0][0]
-    	tob_avg ='{0:.4}'.format(results[0][1])
-    	tob_max =results[0][2]
-    
-    	result_printout =( ['Start_time: ' + start,
-    						'Min_Temperature: '  + str(tob_min),
-    						'Max_Temperature: ' + str(tob_max),
-    						'Average_Temperature: ' + str(tob_avg)])
-    	return jsonify(result_printout)
-
+    valid_start_date = session.query(exists().where(Measurement.date == start)).scalar()
+    if valid_start_date:
+        results = (session.query(func.min(Measurement.tobs),func.max(Measurement.tobs),func.avg(Measurement.tobs)).filter(Measurement.date >= start).all())
+        tob_min = results[0][0]
+        tob_max = results[0][1]
+        tob_avg = "{:.1f}".format(results[0][2])
+        result_printout =( ['Start_time: ' + start, 'Min_Temperature: ' + str(tob_min), 'Max_Temperature: '+ str(tob_max),'Average_Temperature: '+ str(tob_avg)])
+        return jsonify(result_printout)
     return jsonify({"error": f"Input date {start} not valid"}), 404
    
 
 @app.route("/api/v1.0/<start>/<end>")
 def start_end_input(start, end):
-
      # Create our session (link) from Python to the DB
     session = Session(engine)
-
-    # Get date 
-    date_last = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
-    date_last_str = str(date_last)
-    date_last_str = re.sub("'|,", "",date_last_str)
-    print (date_last_str)
-
-    date_first = session.query(Measurement.date).first()
-    date_first_str = str(date_first)
-    date_first_str = re.sub("'|,", "",date_first_str)
-    print (date_first_str)
-
     # Validate start date 
-    valid_date_start = session.query(exists().where(Measurement.date == start)).scalar()
- 	
+    valid_start_date = session.query(exists().where(Measurement.date == start)).scalar()
     # Validate end date 
-    valid_date_end = session.query(exists().where(Measurement.date == end)).scalar()
-
-    if valid_date_start and valid_date_end:
-
-    	results = session.query(func.min(Measurement.tobs),func.avg(Measurement.tobs),func.max(Measurement.tobs)).\
-            filter(Measurement.date >= start).\
-            filter(Measurement.date <= end).all()
-
-    	tob_min =results[0][0]
-    	tob_avg ='{0:.4}'.format(results[0][1])
-    	tob_max =results[0][2]
-
-    	result_printout =( ['Start_time: ' + start,
-                            'End_time ' + end,
-    						'Min_Temperature: '  + str(tob_min),
-    						'Max_Temperature: ' + str(tob_max),
-    						'Average_Temperature: ' + str(tob_avg)])
-    	return jsonify(result_printout)
+    valid_end_date = session.query(exists().where(Measurement.date == end)).scalar()
+    if valid_start_date and valid_end_date:
+        results = session.query(func.min(Measurement.tobs),func.max(Measurement.tobs),func.avg(Measurement.tobs)).filter(Measurement.date >= start).filter(Measurement.date <= end).all()
+        tob_min = results[0][0]
+        tob_max = results[0][1]
+        tob_avg = "{:.1f}".format(results[0][2])
+        result_printout =( ['Start_time: ' + start, 'End_time ' + end, 'Min_Temperature: '  + str(tob_min), 'Max_Temperature: ' + str(tob_max), 'Average_Temperature: ' + str(tob_avg)])
+        return jsonify(result_printout)        
+    return jsonify({"error": f"Input date {start} or {end} not valid"}), 404
 
 if __name__ == "__main__":
     app.run(debug=True)
